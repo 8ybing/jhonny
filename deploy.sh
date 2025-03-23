00efa02d3ebb706cb3bce68f23fbe8ac8cf701ac
8ybing <jhonny22@163.com>
2025-03-23 16:18:26 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,12 @@
 
 echo "Deploying..."
 
+
 path=$(pwd)
 echo "当前文件夹：$path"
 
 echo "开始复制文件..."
-
+exit # 退出容器
 docker cp -r jenkins:$path/* /home/www/self/ 
 if [ $? -eq 0 ]; then
   echo "文件复制完成"
