7b0ab694b7e7a7b0ec9fccf2a101ec185965c564
8ybing <jhonny22@163.com>
2025-03-23 16:15:27 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo "当前文件夹：$path"
 
 echo "开始复制文件..."
 
-sshpass -p "$password" scp -r ./* $username@74.48.213.218:/home/www/self/
+docker cp -r jenkins:$path/* /home/www/self/ 
 if [ $? -eq 0 ]; then
   echo "文件复制完成"
 else
