1a952d105ee31c630faf884161c7eacb19af3514
8ybing <jhonny22@163.com>
2025-03-23 16:07:55 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,15 @@ path=$(pwd)
 echo "当前文件夹：$path"
 
 echo "开始复制文件..."
-cp -r ./* /home/www/self/
-echo "文件复制完成"
+
+sshpass -p "$password" scp -r ./* $username@74.48.213.218:/home/www/self/
+if [ $? -eq 0 ]; then
+  echo "文件复制完成"
+else
+  echo "文件复制失败"
+fi
+
+
+
 
 
